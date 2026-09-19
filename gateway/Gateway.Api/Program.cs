20b775dca1530e98590cf4c41bb3af303b17aa84
Tyrone Vera <tylam.dev@gmail.com>
2026-09-19 09:49:22 -0500

    Condifguracion de reverseproxy para autenticacion

--- gateway/Gateway.Api/Program.cs
+++ gateway/Gateway.Api/Program.cs
@@ -1,9 +1,58 @@
+using Microsoft.IdentityModel.Tokens;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddReverseProxy()
     .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
 
+var jwtKey = builder.Configuration["Jwt:Key"]  ?? throw new InvalidOperationException("JWT Key is no esta configurado.");
+var jwtIssuer = builder.Configuration["Jwt:Issuer"] ?? throw new InvalidOperationException("JWT Issuer is no esta configurado.");
+var jwtAudience = builder.Configuration["Jwt:Audience"] ?? throw new InvalidOperationException("JWT Audience is no esta configurado.");
+var clockSkew = builder.Configuration.GetValue("Jwt:ClockSkewSegundos", 30);
+
+var allowedOrigins = builder.Configuration.GetSection("Cors:AllowedOrigins").Get<string[]>() ?? throw new InvalidOperationException("Allowed Origins is no esta configurado.");
+
+builder.Services.AddAuthentication()
+    .AddJwtBearer("AuthApi", options =>
+    {
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidateIssuerSigningKey = true,
+            ValidateLifetime = true,
+            IssuerSigningKey = new SymmetricSecurityKey(
+                System.Text.Encoding.UTF8.GetBytes(jwtKey)),
+            ValidIssuer = jwtIssuer,
+            ValidAudience = jwtAudience,
+            ClockSkew = TimeSpan.FromSeconds(clockSkew)
+        };
+    });
+
+builder.Services.AddAuthorization(options =>
+{
+    options.AddPolicy("AuthApi", policy =>
+    {
+        policy.AuthenticationSchemes.Add("AuthApi");
+        policy.RequireAuthenticatedUser();
+    });
+});
+
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(builder =>
+    {
+        builder.WithOrigins(allowedOrigins)
+               .AllowAnyHeader()
+               .AllowAnyMethod();
+    });
+});
+
 var app = builder.Build();
 
+app.UseCors();
+app.UseAuthentication();
+app.UseAuthorization();
+app.MapReverseProxy();
 
 app.Run();
