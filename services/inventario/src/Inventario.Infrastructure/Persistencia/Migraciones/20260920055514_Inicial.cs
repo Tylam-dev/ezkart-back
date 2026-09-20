5e3edc673774bad0bdd1927bd78df44c337263cf
Tyrone Vera <tylam.dev@gmail.com>
2026-09-20 00:56:23 -0500

    Creando conexion de base y migracion inicial

---
+++ services/inventario/src/Inventario.Infrastructure/Persistencia/Migraciones/20260920055514_Inicial.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Inventario.Infrastructure.Persistencia.Migraciones
+{
+    /// <inheritdoc />
+    public partial class Inicial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "producto",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    codigo = table.Column<string>(type: "text", nullable: false),
+                    nombre = table.Column<string>(type: "text", nullable: false),
+                    precio = table.Column<decimal>(type: "numeric(13,4)", precision: 13, scale: 4, nullable: false, defaultValue: 0m),
+                    existencia = table.Column<int>(type: "integer", nullable: false, defaultValue: 0),
+                    xmin = table.Column<uint>(type: "xid", rowVersion: true, nullable: false),
+                    fecha_creacion = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    fecha_actualizacion = table.Column<DateTime>(type: "timestamp with time zone", nullable: true),
+                    fecha_eliminacion = table.Column<DateTime>(type: "timestamp with time zone", nullable: true),
+                    estado = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_producto", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "producto");
+        }
+    }
+}
