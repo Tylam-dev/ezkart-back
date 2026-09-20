63b1b9f2f1ef3b838a526b4d42c1b804cf654d4f
Tyrone Vera <tylam.dev@gmail.com>
2026-09-20 12:49:39 -0500

    Creando metodos p[ara opbtener informacin en el servicio de compras

--- services/compras/src/Compras.Application/Servicio/OrdenesServicio.cs
+++ services/compras/src/Compras.Application/Servicio/OrdenesServicio.cs
@@ -1,20 +1,71 @@
+using Compras.Application;
 using Compras.Application.DTOs;
+using Compras.Application.Exepciones;
 using Compras.Application.Utilidades;
 using Compras.Application.Utilidades.Queries;
 using Compras.Domain.Dominio;
+using Compras.Domain.Exepcion;
 
 public class OrdenesServicio : IOrdenesServicio
 {
-    public Task<Orden> FinalizarCompra(Guid usuarioId)
+    private readonly IRepositorioCompras _repositorioCompras;
+    private readonly IRepositorioCarrito _repositorioCarrito;
+    private readonly IClienteHttpInventario _clienteHttpInventario;
+    public OrdenesServicio(
+        IRepositorioCompras repositorioCompras,
+        IRepositorioCarrito repositorioCarrito,
+        IClienteHttpInventario clienteHttpInventario
+    )
     {
+        _repositorioCompras = repositorioCompras;
+        _repositorioCarrito = repositorioCarrito;
+        _clienteHttpInventario = clienteHttpInventario;
     }
-    public Task<Paginacion<OrdenDTO>> ObtenerOrdenesPaginado(
+    public async Task<Orden> FinalizarCompra(Guid usuarioId)
+    {
+
+    }
+    public async Task<Paginacion<OrdenDTO>> ObtenerOrdenesPaginado(
         Guid usuarioId,
         PaginacionQuery<OrdenesQuery> query
     )
     {
+        var ordenes = await _repositorioCompras.ObtenerOrdenesPaginado(usuarioId, query);
+
+        if(!ordenes.Exitoso) throw ordenes.Excepcion!;
+
+        return MapearOrdenesADTO(ordenes.Valor!);
     }
-    public Task<Orden> ObtenerOrdenPorId(Guid usuarioId, Guid ordenId)
+    public async Task<Orden> ObtenerOrdenPorId(Guid usuarioId, Guid ordenId)
     {
+        var orden = await _repositorioCompras.ObtenerOrdenPorId(usuarioId, ordenId);
+
+        if(!orden.Exitoso) throw orden.Excepcion!;
+
+        if(orden.Valor is null) throw new ExepcionOrdenNoEncontrada();
+
+        return orden.Valor;
+    }
+    private static Paginacion<OrdenDTO> MapearOrdenesADTO(Paginacion<Orden> ordenes)
+    {
+        var ordenesDTO = new List<OrdenDTO>();
+
+        foreach(var orden in ordenes.Items) ordenesDTO.Add(new OrdenDTO()
+        {
+            Id = orden.Id,
+            FechaCreacion = orden.FechaCreacion,
+            EstadoOrden = orden.EstadoOrden,
+            Total = orden.Total,
+            DescuentoTemporadaId = orden.DescuentoTemporada?.Id
+        });
+
+        return new Paginacion<OrdenDTO>()
+        {
+            Items = ordenesDTO,
+            Pagina = ordenes.Pagina,
+            TamanoPagina = ordenes.TamanoPagina,
+            TotalItems = ordenes.TotalItems,
+            TotalPaginas = ordenes.TotalPaginas
+        };
     }
 }
