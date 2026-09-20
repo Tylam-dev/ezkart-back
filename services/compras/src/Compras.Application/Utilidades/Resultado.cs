32b49bcd6e2cc54d6f39bade3ccca9a48db13b6e
Tyrone Vera <tylam.dev@gmail.com>
2026-09-20 09:08:54 -0500

    Creando utilidades en compras

---
+++ services/compras/src/Compras.Application/Utilidades/Resultado.cs
@@ -0,0 +1,19 @@
+namespace Compras.Application.Utilidades;
+
+public class Resultado<T>
+{
+    public bool Exitoso { get; set; }
+    public T? Valor { get; set; }
+    public string? MensajeError { get; set; }
+    public Exception? Excepcion { get; set; }
+
+    public static Resultado<T> Exito(T valor)
+    {
+        return new Resultado<T> { Exitoso = true, Valor = valor };
+    }
+
+    public static Resultado<T> Error(string mensajeError, Exception? ex)
+    {
+        return new Resultado<T> { Exitoso = false, MensajeError = mensajeError, Excepcion = ex};
+    }
+}
